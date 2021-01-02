64c301999e1ac1549ba39ca217640662f9d3e558
Luo-DH <974947011@qq.com>
2021-01-02 15:53:08 +0800

    init

---
+++ view/src/main/java/com/luo/view/TestView.kt
@@ -0,0 +1,20 @@
+package com.luo.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.TextView
+import org.w3c.dom.Text
+
+class TestView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : LinearLayout(context, attrs, defStyleAttr) {
+
+    init {
+        LayoutInflater.from(context).inflate(
+            R.layout.head, this, true
+        )
+    }
+}
